bcaa9af3739238687317c8fbbd8e4c367bc72924
Marcel <marcel.grimm@live.de>
2019-10-01 09:02:43 +0200

    revised/ refactored whole project added viewModel for MainView

---
+++ Cryptography.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Konfiguration_WPF
+{
+
+
+    public static class Cryptography
+    {
+        private const string EncryptionKey = "test123456key";
+
+        //TODO: clarify if following method is useless / duplicate
+        public static string EncryptData(string strOriginal)
+        {
+            try
+            {
+                string EncryptionKey = "test123456key";
+                byte[] clearBytes = Encoding.Unicode.GetBytes(strOriginal);
+                using (Aes encryptor = Aes.Create())
+                {
+                    Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                    encryptor.Key = pdb.GetBytes(32);
+                    encryptor.IV = pdb.GetBytes(16);
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                        {
+                            cs.Write(clearBytes, 0, clearBytes.Length);
+                            cs.Close();
+                        }
+                        strOriginal = Convert.ToBase64String(ms.ToArray());
+                    }
+                }
+                return strOriginal;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// Encrypts given string with AES and returns it as bas64string
+        /// </summary>
+        /// <param name="toEncrypt">String to encrypt</param>
+        /// <returns>Returns given string as encrypted base64String </returns>
+        public static string Encrypt(string toEncrypt)
+        {
+            try
+            {
+                //TODO: clarify if const encryption key should be used 
+                var encryptionKey = SystemInformation.Encryption_Key();
+                var clearBytes = Encoding.Unicode.GetBytes(toEncrypt);
+
+                using (var encryptionObject = Aes.Create())
+                {
+                    var salt = new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 };
+                    var rfc2898DeriveBytes = new Rfc2898DeriveBytes(encryptionKey, salt);
+
+                    encryptionObject.Key = rfc2898DeriveBytes.GetBytes(32);
+                    encryptionObject.IV = rfc2898DeriveBytes.GetBytes(16);
+
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        using (var cryptoStream = new CryptoStream(memoryStream, encryptionObject.CreateEncryptor(), CryptoStreamMode.Write))
+                        {
+                            cryptoStream.Write(clearBytes, 0, clearBytes.Length);
+                            cryptoStream.Close();
+                        }
+
+                        toEncrypt = Convert.ToBase64String(memoryStream.ToArray());
+                    }
+                }
+                return toEncrypt;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        
+        /// <summary>
+        /// Decrypts given encrypted base64string with AES and returns it as string
+        /// </summary>
+        /// <param name="toDecrypt">Base64String to decrypt</param>
+        /// <returns>Returns given base64string as decrypted string</returns>
+        public static string Decrypt(string toDecrypt)
+        {
+         
+            try
+            {
+                var cipherBytes = Convert.FromBase64String(toDecrypt);
+
+                using (var encryptionObject = Aes.Create())
+                {
+                    var salt = new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 };
+                    var rfc2898DeriveBytes = new Rfc2898DeriveBytes(EncryptionKey, salt);
+
+                    encryptionObject.Key = rfc2898DeriveBytes.GetBytes(32);
+                    encryptionObject.IV = rfc2898DeriveBytes.GetBytes(16);
+
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        using (var cryptoStream = new CryptoStream(memoryStream, encryptionObject.CreateDecryptor(), CryptoStreamMode.Write))
+                        {
+                            cryptoStream.Write(cipherBytes, 0, cipherBytes.Length);
+                            cryptoStream.Close();
+                        }
+                        toDecrypt = Encoding.Unicode.GetString(memoryStream.ToArray());
+                    }
+                }
+                return toDecrypt;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+    }
+}
---
+++ configuratorViewModel.cs
@@ -0,0 +1,74 @@
+﻿namespace Konfiguration_WPF
+{
+    public class ConfiguratorViewModel
+    {
+        public string ProcessorID             { get; set; } = string.Empty;
+
+        public string CpuName                 { get; set; } = string.Empty;
+
+        public string SerialNumber            { get; set; } = string.Empty;
+                                              
+        public string Manufacturer            { get; set; } = string.Empty;
+
+        public string Modell1                 { get; set; } = string.Empty;
+                                              
+        public string Modell2                 { get; set; } = string.Empty;
+
+        public int CPUBit                     { get; set; } = 0;
+
+        public string RamType                 { get; set; } = string.Empty;
+
+        public double TotalPhysicalMemory     { get; set; } = 0;
+
+        public int PhysicalMemoryCount        { get; set; } = 0;
+
+        public string RamSpeed                { get; set; } = string.Empty;
+
+        public string HDD1                    { get; set; } = string.Empty;
+                                              
+        public string HDD2                    { get; set; } = string.Empty;
+                                              
+        public string HDD3                    { get; set; } = string.Empty;
+
+        public string GraphicCard             { get; set; } = string.Empty;
+
+        public string GraphicCardResolution   { get; set; } = string.Empty;
+
+        public string Mainboard               { get; set; } = string.Empty;
+                                              
+        public string RomDrive1               { get; set; } = string.Empty;
+                                              
+        public string RomDrive2               { get; set; } = string.Empty;
+                                              
+        public string OsVersion               { get; set; } = string.Empty;
+                                              
+        public string SystemTyp               { get; set; } = string.Empty;
+
+        public string Email_PasswordEncrypted { get; set; } = string.Empty;
+
+        public static ConfiguratorViewModel BuildConfiguratorViewModel()
+        {
+            return new ConfiguratorViewModel
+            {
+                SystemTyp             = SystemInformation.SystemTyp_String(),
+                CpuName               = SystemInformation.CPU_NAME(),
+                CPUBit                = SystemInformation.CPU_BIT(),
+                Manufacturer          = SystemInformation.Manufacturer(),
+                Modell1               = SystemInformation.Modell1(),
+                Modell2               = SystemInformation.Modell2(),
+                HDD1                  = SystemInformation.HDD1(),
+                HDD2                  = SystemInformation.HDD2(),
+                HDD3                  = SystemInformation.HDD3(),
+                RamType               = SystemInformation.RAM_TYP(),
+                TotalPhysicalMemory   = SystemInformation.TotalPhysicalMemory(),
+                RamSpeed              = SystemInformation.RAM_SPEED(),
+                SerialNumber          = SystemInformation.SerialNumber(),
+                GraphicCard           = SystemInformation.GraphicCard(),
+                GraphicCardResolution = SystemInformation.GraphicCardResolution(),
+                RomDrive1             = SystemInformation.RomDrive1(),
+                RomDrive2             = SystemInformation.RomDrive2(),
+                OsVersion             = SystemInformation.GetWindowsClientVersion()
+            };
+        }
+    }
+}
